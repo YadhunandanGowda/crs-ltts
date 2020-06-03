8e541056fc0c18bd5ab84cba941b478cd4631fe7
Yadhunandana Gowda B S <yadhunandana.gowda@regalix.com>
2020-06-03 17:57:45 +0530

    created new dash board

--- public/js/script.js
+++ public/js/script.js
@@ -1,53 +1,80 @@
 $(document).ready(function () {
-    
-    // Header JS Starts 
+  // Header JS Starts
 
-    $('#app-logo').on('click', function () {
-        $(this).addClass('hide');
-        $(this).siblings('.options').addClass('show');
-    })
+  $("#app-logo").on("click", function () {
+    $(this).addClass("hide");
+    $(this).siblings(".options").addClass("show");
+  });
 
-    $('header .options .profile-pic .outer-pp').on('click', function () {
-        $(this).siblings('.logout-popup').fadeToggle();
-        $(this).parents('.profile-pic').toggleClass('active');
-    })
+  $("header .options .profile-pic .outer-pp").on("click", function () {
+    $(this).siblings(".logout-popup").fadeToggle();
+    $(this).parents(".profile-pic").toggleClass("active");
+  });
 
-    $('header .options .profile-pic .logout-popup .cls-btn').on('click', function () {
-        $(this).parents('.profile-pic').removeClass('active');
-        $(this).parents('.logout-popup').fadeOut();
-    })
+  $("header .options .profile-pic .logout-popup .cls-btn").on(
+    "click",
+    function () {
+      $(this).parents(".profile-pic").removeClass("active");
+      $(this).parents(".logout-popup").fadeOut();
+    }
+  );
 
+  $(document).mouseup(function (e) {
+    var header = $("header .options .profile-pic .logout-popup");
+    if (
+      !$("header .options .profile-pic img").is(e.target) &&
+      !$("header .options .profile-pic").is(e.target) &&
+      !header.is(e.target) &&
+      header.has(e.target).length === 0
+    ) {
+      header.parents(".profile-pic").removeClass("active");
+      header.fadeOut();
+    }
+  });
+  $(".burger").click(function () {
+    $(".burger").toggleClass("clicked");
+    $("nav.cstm-nav-bar").toggleClass("show");
+  });
 
-    $(document).mouseup(function (e) {
-        var container = $("header .options .profile-pic .logout-popup");
-        if (!$('header .options .profile-pic img').is(e.target) && !$('header .options .profile-pic').is(e.target) && !container.is(e.target) && container.has(e.target).length === 0) {
-            container.parents('.profile-pic').removeClass('active');
-            container.fadeOut();
-        }
-    });
-    $('.burger').click(function(){
-        $('.burger').toggleClass('clicked');
-        // $('.overlay').toggleClass('show');
-        $('nav.cstm-nav-bar').toggleClass('show');
-      });
+  $(".cstm-nav-bar .multi > a").click(function () {
+    $(this).parents(".multi").toggleClass("clicked");
+    $(this).parents(".multi").children("ul").toggleClass("show");
+  });
 
-      $(".cstm-nav-bar .multi > a").click(function(){
-        $(this).parents('.multi').toggleClass('clicked');
-        $(this).parents('.multi').children('ul').toggleClass('show');
-      });
+  // Header JS Ends
 
-    // Header JS Ends 
+  // draft page JS starts
 
-})
+  $(" .banner-sec .bnr-img-sec").slick({
+    dots: false,
+    prevArrow: false,
+    nextArrow: false,
+    draggable: false,
+    asNavFor: ".banner-sec .bnr-cnt-sec",
+    autoplay: true,
+    autoplaySpeed: 4000,
+  });
+
+  $(" .banner-sec .bnr-cnt-sec").slick({
+    dots: true,
+    prevArrow:
+      "<div class='arrow-lft'><img class='slick-prev' src='./images/arrow-left.svg'></div>",
+    nextArrow:
+      "<div class='arrow-rgt'><img class='slick-next' src='./images/arrow-right.svg'></div>",
+    draggable: false,
+    asNavFor: ".banner-sec .bnr-img-sec",
+  });
+
+  // draft page JS ends
+});
 
 document.onreadystatechange = function () {
-    var state = document.readyState
-    if (state == 'interactive') {
-        //  document.getElementById('contents').style.visibility="hidden";
-    } else if (state == 'complete') {
-        setTimeout(function () {
-            document.getElementById('preloader').style.display = "none";
-        }, 1000);
-    }
-}
-
+  var state = document.readyState;
+  if (state == "interactive") {
+    //  document.getElementById('contents').style.visibility="hidden";
+  } else if (state == "complete") {
+    setTimeout(function () {
+      document.getElementById("preloader").style.display = "none";
+    }, 1000);
+  }
+};
